41ee4b17365488baa98a8193ce49e28ff455c952
Вдадимир Лызь <lyz@sfedu.ru>
2019-12-14 14:15:28 +0300

    HT-3: classes and interfaces

---
+++ Application/Controllers/GroupController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core.Services.Interfaces;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupController : Controller
+    {
+        private IGroupService _groupService;
+
+        public GroupController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpGet("[action]")]
+        public void CreateGroup()
+        {
+            _groupService.CreateGroup();
+        }
+
+        [HttpGet("[action]")]
+        public void DeleteGroup()
+        {
+            _groupService.DeleteGroup();
+        }
+
+        [HttpGet("[action]")]
+        public void AddParticipant()
+        {
+            _groupService.AddParticipant();
+        }
+
+        [HttpGet("[action]")]
+        public void RemoveParticipant()
+        {
+            _groupService.RemoveParticipant();
+        }
+
+        [HttpGet("[action]")]
+        public void AddContest()
+        {
+            _groupService.AddContest();
+        }
+
+        [HttpGet("[action]")]
+        public void RemoveContest()
+        {
+            _groupService.RemoveContest();
+        }
+
+    }
+}
---
+++ Core/Services/Interfaces/IGroupService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Services.Interfaces
+{
+    public interface IGroupService
+    {
+        void CreateGroup();
+
+        void DeleteGroup();
+
+        void AddParticipant();
+
+        void RemoveParticipant();
+
+        void AddContest();
+
+        void RemoveContest();
+    }
+}
